ae58898cc70ea9c9741490929f0c42eee572799a
assmaabaaj <elsabaaj@gmail.com>
2026-07-14 16:18:19 +0300

    Create p43.cpp

---
+++ p43.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArray(int arr[100], int &arrLength)
+{
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumber(1, 100);
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+
+    cout << "\n";
+}
+
+int EvenCount(int arr[100], int arrLength)
+{
+    int Counter = 0;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            Counter++;
+        }
+    }
+
+    return Counter;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength;
+
+    FillArray(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nEven Numbers count is: ";
+    cout << EvenCount(arr, arrLength) << endl;
+
+    return 0;
+}
